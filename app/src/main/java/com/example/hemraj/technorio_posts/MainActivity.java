ac6bc294326d50fe0627e3e3ef4590429a57a038
Hemraj Rijal <her0139@my.londonmet.ac.uk>
2017-02-07 11:01:35 +0545

    Main Layout of the App (Main Screen)

--- app/src/main/java/com/example/hemraj/technorio_posts/MainActivity.java
+++ app/src/main/java/com/example/hemraj/technorio_posts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hemraj.technorio_posts;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button camera = (Button) findViewById(R.id.post_view);
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button list = (Button) findViewById(R.id.tag_view);
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
+

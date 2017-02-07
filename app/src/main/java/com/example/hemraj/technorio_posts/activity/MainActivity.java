e39afae4282ff5d3e569153d3f8fddf0939429fb
Hemraj Rijal <her0139@my.londonmet.ac.uk>
2017-02-07 11:10:05 +0545

    New launcher Icon

--- app/src/main/java/com/example/hemraj/technorio_posts/activity/MainActivity.java
+++ app/src/main/java/com/example/hemraj/technorio_posts/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.hemraj.technorio_posts;
+package com.example.hemraj.technorio_posts.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.hemraj.technorio_posts.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button camera = (Button) findViewById(R.id.post_view);
-        camera.setOnClickListener(new View.OnClickListener() {
+        Button post = (Button) findViewById(R.id.post_view);
+        post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
 
-        Button list = (Button) findViewById(R.id.tag_view);
-        list.setOnClickListener(new View.OnClickListener() {
+        Button tag = (Button) findViewById(R.id.tag_view);
+        tag.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
